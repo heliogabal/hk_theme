3fa1f558b110b786e34e5159f4e91c97e415185d
heliogabal <rhalbmann@gmail.com>
2015-04-09 15:08:57 +0200

    Footer

--- templates/region--footer.tpl.php
+++ templates/region--footer.tpl.php
@@ -10,7 +10,7 @@
 <?php if ($content): ?>
   <footer class="<?php print $classes; ?>">
       <a href="/" title="Home">
-        <img width="256" height="48" alt="" src="/sites/all/themes/hk_theme/images/signet_bg.png" class="media-element file-default">
+        <img width="256" height="48" alt="" src="/sites/all/themes/hk_theme/images/signet_weiss.png" class="media-element file-default">
       </a>
     <?php print $content; ?>
   </footer>
