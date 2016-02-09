bfdc95969e330a2043b774ad31291da183529b4d
heliogabal <rhalbmann@gmail.com>
2016-02-10 00:43:24 +0100

    fix mobile menu abstand

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -6,7 +6,6 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
 
 <head profile="<?php print $grddl_profile; ?>">
-  <script type="text/javascript" src="http://fast.fonts.net/jsapi/69c28897-3128-41dd-a024-e20f5106e17e.js"></script>
   <meta name="google-site-verification" content="knQSpp_mc-a5nAUxUS_QaBDLS1tddahIn2dIhhGQ4EM" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
@@ -32,6 +31,7 @@
     <![endif]-->
   <?php endif; ?>
   <script>jQuery.noConflict();</script>
+  <script type="text/javascript" src="http://fast.fonts.net/jsapi/69c28897-3128-41dd-a024-e20f5106e17e.js"></script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php if ($skip_link_text && $skip_link_anchor): ?>
